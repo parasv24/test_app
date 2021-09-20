c394f85d5c140e8fe2d885025d5b759d0b26acf1
paras <paras@veersatech.com>
2021-09-20 15:37:29 +0530

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,35 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        respond_to do |format|
+            @article.update(params.require(:article).permit(:title, :description, :author))
+            format.html { redirect_to @article, notice: "Article was successfully updated." }
+        end
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        respond_to do |format|
+            @article.destroy
+            format.html { redirect_to articles_url, notice: "Article was successfully updated." }
+        end
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description, :author))
+        respond_to do |format|
+            @article.save
+            format.html { redirect_to @article, notice: "Article was successfully created." }
+        end
+    end
+    def new 
+        @article = Article.new
+    end
+end
---
+++ db/migrate/20210920051144_adding.rb
@@ -0,0 +1,5 @@
+class Adding < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles , :author, :string
+  end
+end
